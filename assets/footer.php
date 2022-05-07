827f579c187e6816f02734f81175de9d672bb1d2
developeryoo <77091347+developeryoo@users.noreply.github.com>
2022-05-07 07:08:28 +0300

    Add files via upload

---
+++ assets/footer.php
@@ -0,0 +1,25 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer__menu">
+            <div class="footer__contacts">
+                <a href="#">Новости</a>
+                <a href="#">Мессенджер</a>
+                <a href="#">Сообщества</a>
+                <a href="#">Документы</a>
+                <a href="#">Фотографии</a>
+            </div>
+            <div class="footer__contacts__2">
+                <a href="#">Контакты</a>
+                <a href="#">Видеохостинг</a>
+                <a href="#">Видеоконференции</a>
+                <a href="#">Курсы и мастер классы</a>
+                <a href="#">Учебные материалы</a>
+            </div>
+            <div class="footer__contacts__3">
+                <a href="tel:89000000">+7(900)000-00-00</a>
+                <p>2022 г., Республика Саха (Якутия), Ус Тумсуу</p>
+                <button class="footer__button">Задать вопрос</button>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ assets/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="header__logo">
+                <img class="logo" src="img/logo2.png" alt="">
+            </div>
+            <nav class="nav">
+                <a class="nav__link" href="index.php">Новости</a>
+                <a class="nav__link" href="literature.php">Мессенджер</a>
+                <a class="nav__link" href="map.php">Сообщество</a>
+                <a class="nav__link" href="map.php">Документы</a>
+                <a class="nav__link" href="map.php">Другое</a>
+            </nav>
+        </div>
+    </div>
+</header>
