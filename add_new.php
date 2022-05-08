03bcd0684ff6008ff319d224d4ac83bc2774d905
developeryoo <77091347+developeryoo@users.noreply.github.com>
2022-05-08 06:07:54 +0300

    Delete add_new.php

--- add_new.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conn = mysqli_connect('localhost', 'root', '', 'ustumsuu');
-	$query_text = "INSERT INTO news (date, type, title, img, text) VALUES ('{$_POST['post_date']}', '{$_POST['post_type']}', '{$_POST['post_title']}', '{$_POST['post_image']}', '{$_POST['post_text']}')";
-	$query = mysqli_query($conn, $query_text);
-
-	header("Location: index.php");
-?>
